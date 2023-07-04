ed0c61279e30fc9ff3aeafdc9e7e36c329858251
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2023-07-04 01:18:04 +0100

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 from flask import Flask, request
 from flask_restful import Resource, Api
 import pprint
@@ -7,19 +8,21 @@ import random
 app = Flask(__name__)
 api = Api(app)
 
-qoutes_json = None
-
-with open('quote.json', 'r') as qts:
+# qoutes_json = None
+qoute_db = 'new_qoutes.json'
+with open(qoute_db, 'r') as qts:
     qoutes_json = json.loads(qts.read())
 
 
 def write_new_qoute(data):
     new = []
-    with open('new_qoutes.json', 'r') as qts:
+    with open(qoute_db, 'r') as qts:
         existing_qoutes = qts.read()
         new = json.loads(existing_qoutes) if existing_qoutes else []
-        new.append(json.loads(data))
-        json.dump(new, open('new_qoutes.json', 'w'), indent=2)
+        data = json.loads(data)
+        data['id'] = new[-1]['id']+1
+        new.append(data)
+        json.dump(new, open(qoute_db, 'w'), indent=2)
 
 
 def qoute_by_id(id_):
@@ -42,6 +45,16 @@ def get_random_qoute():
     return random.choice(qoutes_json)
 
 
+def delete_qoute(id_):
+    updated_qoutes = []
+    with open(qoute_db, 'r') as qts:
+        existing_qoutes = qts.read()
+        existing_qoutes = json.loads(existing_qoutes)
+        updated_qoutes = filter(
+            lambda x: x['id'] != id_, existing_qoutes)
+        json.dump(list(updated_qoutes), open(qoute_db, 'w'), indent=2)
+
+
 class Qoute(Resource):
     def get(self):
         return get_random_qoute()
@@ -58,18 +71,41 @@ class QouteID(Resource):
         return qoute_by_id(qoute_id)
 
     def delete(self, qoute_id):
+        delete_qoute(qoute_id)
+        return "Success", 201
 
-        return
+
+class New(Resource):
+    def post(self):
+        data = request.form['data']
+        print(request.form)
+        write_new_qoute(data)
+        return "sucess", 201
 
 
+class Hello(Resource):
+    def get(self):
+        return """
+        GET - <strong>/random</strong> get a random qoute
+        POST - <strong>/random</strong> author="name" get random qoute from author
+        POST - <strong>/new</strong> data={} post new qoute to database
+        GET  - <strong>/:id</strong> get qoute with specific ID
+        DELETE - <strong>/:id</strong> delte qoute with specific ID
+        """
+
+
+api.add_resource(Hello, '/')
 api.add_resource(Qoute, '/random')
+api.add_resource(New, '/new')
 api.add_resource(QouteID, '/<int:qoute_id>')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
     # write_new_qoute(
     #     {'author': 'abdul', 'quote': 'Observation is the secret to discovery!'})
     # pprint.pprint(get_qoutes_by_author('Steve Jobs'))
     # add_id()
     # print(qoute_by_id('1'))
+    # delete_qoute(3)
